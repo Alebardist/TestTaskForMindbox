048be4c9a6a51b8d0d1e500371bb851b4f789fe4
Michel R <52571455+Alebardist@users.noreply.github.com>
2022-11-06 20:48:10 +0300

    Added new figure and renamed file with unit-tests

---
+++ Triangle.cs
@@ -0,0 +1,37 @@
+﻿namespace TestTaskForMindbox
+{
+    internal class Triangle : Figure
+    {
+        /// <summary>
+        /// Checks if Triangle is Rectangular by Pifagor's theoreme
+        /// </summary>
+        public bool IsRectangular
+        {
+            get
+            {
+                if ((FirstSide * FirstSide + SecondSide * SecondSide == ThirdSide * ThirdSide) ||
+                    (FirstSide * FirstSide + ThirdSide * ThirdSide == SecondSide * SecondSide) ||
+                    (ThirdSide * ThirdSide + SecondSide * SecondSide == FirstSide * FirstSide))
+                    return true;
+                return false;
+            }
+        }
+
+        public float FirstSide { get; set; }
+        public float SecondSide { get; set; }
+        public float ThirdSide { get; set; }
+
+        public override float CalculateSquare()
+        {
+            float square = 0;
+
+            if (IsRectangular)
+            {
+                //TODO: need to pick a pair of the shortest sides
+                square = (FirstSide * SecondSide) / 2; //Division by 2, because of formulae S = 0.5*(a*b)
+            }
+
+            return (FirstSide * SecondSide * ThirdSide)/2; //Division by 2, because of formulae S = 0.5*(a*b*c)
+        }
+    }
+}
